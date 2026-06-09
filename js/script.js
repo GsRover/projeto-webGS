ca946bb17ec92693f991b589b3d2dfe02f92b345
Guilherme Nascimento <borutoplays038@gmail.com>
2026-06-09 02:44:35 -0300

    feat: Adição do slideshow na seção PROBLEMA

--- js/script.js
+++ js/script.js
@@ -7,3 +7,30 @@ function mudarTema(tema) {
         document.body.classList.add(`tema-${tema}`);
     }
 }
+
+// --- LÓGICA DO SLIDESHOW ---
+let slideIndex = 1;
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (document.getElementsByClassName("slide").length > 0) {
+        mostrarSlides(slideIndex);
+    }
+});
+
+function mudarSlide(n) {
+    mostrarSlides(slideIndex += n);
+}
+
+function mostrarSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    
+    if (n > slides.length) { slideIndex = 1 }    
+    if (n < 1) { slideIndex = slides.length }
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slides[slideIndex - 1].style.display = "block";  
+}
